3c209c5bfae821fcd4e0223682cd8cd479aecf1a
leongjinqwen <leongjinqwen@hotmail.com>
2019-03-29 18:08:00 +0800

    fix csrf for api

--- instagram_api/__init__.py
+++ instagram_api/__init__.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app,csrf
 from flask_cors import CORS
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -7,6 +7,10 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 from instagram_api.blueprints.users.views import users_api_blueprint
 from instagram_api.blueprints.images.views import images_api_blueprint
 from instagram_api.blueprints.sessions.views import sessions_api_blueprint
+
+csrf.exempt(users_api_blueprint)
+csrf.exempt(images_api_blueprint)
+csrf.exempt(sessions_api_blueprint)
 
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
 app.register_blueprint(images_api_blueprint, url_prefix='/api/v1/images')
