857fa9805cac504fae036ef57bb4b8da642f038c
leongjinqwen <leongjinqwen@hotmail.com>
2019-06-25 15:41:53 +0800

    add api endpoint for feeds and checkname

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,jsonify,request,make_response
 from models.user import User
+from models.image import Image
 
 users_api_blueprint = Blueprint('users_api',
                              __name__,
@@ -15,7 +16,10 @@ def index():
             'username': user.username,
             'email': user.email,
             'bio': user.bio,
-            'profileImage' : user.profile_image_url
+            'profileImage' : user.profile_image_url,
+            'total_images' : len(user.images),
+            'fans' : len(user.fans),
+            'idols' : len(user.idols)
         }
         result.append(each)
     return jsonify(result)
@@ -63,3 +67,34 @@ def create():
 
             return make_response(jsonify(responseObject)), 201
     
+@users_api_blueprint.route('/feeds', methods=['GET'])
+def feeds():
+    images =Image.select().order_by(Image.created_at.desc())
+    result = []
+    for image in images:
+        each = {
+            'id': image.user.id,
+            'image_url': image.image_url,
+            'user': image.user.username,
+            'created_at': image.created_at,
+            'user_profileImage' : image.user.profile_image_url
+        }
+        result.append(each)
+    return jsonify(result)
+
+    
+@users_api_blueprint.route('/check_name/<newname>', methods=['GET'])
+def check_name(newname):
+    result = User.get_or_none(User.username == newname )
+    if result == None :
+        responseObject = {
+            'valid': True,
+            'exist': False
+        }
+        return make_response(jsonify(responseObject)), 201
+    
+    responseObject = {
+        'valid': False,
+        'exist': True
+    }
+    return make_response(jsonify(responseObject)), 400
